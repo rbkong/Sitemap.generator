d2ad0c3fcb2ec0677f26821178052afbe909d3bd
Ronald Kongi <rk9.ronaldo@gmail.com>
2020-03-16 09:36:12 -0400

    Complete Html class

--- Sitemap.Generator/Sitemap.Generator/Html.cs
+++ Sitemap.Generator/Sitemap.Generator/Html.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Sitemap.Generator
 {
     class Html
     {
+        private string _content;
+        private string _url;
+
+        public Html(string url)
+        {
+            _url = url;
+        }
+
+        public async Task GetContent()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage msg = await client.GetAsync(_url);
+                msg.EnsureSuccessStatusCode();
+                _content = await msg.Content.ReadAsStringAsync();
+                _content = Regex.Replace(_content, @"\t\n\r", "");
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
