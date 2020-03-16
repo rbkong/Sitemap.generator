f923b4f3b9df02a783ef1cd2cda32951e1fd0613
Ronald Kongi <rk9.ronaldo@gmail.com>
2020-03-16 15:49:35 -0400

    Node file Complete

---
+++ Sitemap.Generator/Sitemap.Generator/Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sitemap.Generator
+{
+    public class Node
+    {
+        public string Link { get; set; }
+        public int Index { get; set; }
+
+        public Node(string link, int index)
+        {
+            Link = link; Index = index;
+        }
+        public static bool operator ==(Node a, Node b)
+        {
+            return a.Link.Equals(b.Link);
+        }
+
+        public static bool operator !=(Node a, Node b)
+        {
+            return !a.Link.Equals(b.Link);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+    }
+}
