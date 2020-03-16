41304be201c9a5d896df8f0e3b60ebb965ec6482
Ronald Kongi <rk9.ronaldo@gmail.com>
2020-03-16 16:06:48 -0400

    Html parser Complete

--- Sitemap.Generator/Sitemap.Generator/HtmlParser.cs
+++ Sitemap.Generator/Sitemap.Generator/HtmlParser.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Sitemap.Generator
 {
     public static class HtmlParser
     {
-        public static List<string> GetTags(string webcontent)
+        private static string pattern_tag = @"\w*a.*?href=(""|')(?<ref>.*?)(""|').*?>(.*?)<\w/a\w*>";
+        public static List<Node> GetTags(string webcontent, int index, string url)
         {
             List<string> tags = new List<string>();
-            string pattern_tag = @"\w*a.*?href=(""|')(?<ref>.*?)(""|').*?>(.*?)<\w/a\w*>";
-            return null;
+            List<Node> nodes = new List<Node>();
+            MatchCollection matches = Regex.Matches(webcontent, pattern_tag);
+            foreach(Match match in matches)
+            {
+                string result = (match.Groups["ref"]).ToString();
+                result = result.Trim();
+
+                if (result[^1] != '/') result += "/";
+                if(result[0] == '/' && !tags.Contains(result))
+                {
+                    result = url + result;
+                }
+            }
+            tags.ForEach(lk => nodes.Add(new Node(lk, index)));
+            return nodes;
         }
     }
 }
+
