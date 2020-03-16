5739679349420e6c8f876debae397fb2b9a56cc6
Ronald Kongi <rk9.ronaldo@gmail.com>
2020-03-16 09:20:52 -0400

    Adding necessary files for the projects

---
+++ Sitemap.Generator/Sitemap.Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sitemap.Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
