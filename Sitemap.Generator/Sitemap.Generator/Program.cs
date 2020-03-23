43f84b481d7630667626559885d217c917509db5
Ronald Kongi <rk9.ronaldo@gmail.com>
2020-03-23 11:36:28 -0400

    Finishing up the sitemap generator

--- Sitemap.Generator/Sitemap.Generator/Program.cs
+++ Sitemap.Generator/Sitemap.Generator/Program.cs
@@ -11,48 +11,62 @@ namespace Sitemap.Generator
         {
             Queue<Node> activeQueue = new Queue<Node>();
             List<string> container = new List<string>();
-            Html html = new Html(args[0]);
-            await html.Process();
-            List<string> temp = HtmlParser.GetTags(html.Content(), args[0]);
-            container.AddRange(temp);
-            foreach (var item in container)
+            
+            if (args.Length > 0)
             {
-                activeQueue.Enqueue(new Node(item));
-            }
-            container = container.Distinct().ToList();
-            container.Sort();
-            while (activeQueue.Count > 0)
-            {
-                try
+                Html html = new Html(args[0]);
+                Console.WriteLine($"Process as started ...\n");
+                await html.Process();
+                List<string> temp = HtmlParser.GetTags(html.Content(), args[0]);
+                container.AddRange(temp);
+                foreach (var item in container)
                 {
-                    Node node = activeQueue.Dequeue();
-                    //Console.WriteLine("Dequeue::" + node.Index.ToString() +"::"+ node.Link);
-                    Html childhtml = new Html(node.Link);
-                    await childhtml.Process();
-                    List<string> tempNodes = HtmlParser.GetTags(childhtml.Content(),
-                        "http://specialprojects.com");
-                    tempNodes.ForEach(nd =>
+                    activeQueue.Enqueue(new Node(item));
+                }
+                container = container.Distinct().ToList();
+                container.Sort();
+                while (activeQueue.Count > 0)
+                {
+                    try
                     {
-                        if (!container.Contains(nd))
+                        Node node = activeQueue.Dequeue();
+                        //Console.WriteLine("Dequeue::" + node.Index.ToString() +"::"+ node.Link);
+                        Html childhtml = new Html(node.Link);
+                        await childhtml.Process();
+                        List<string> tempNodes = HtmlParser.GetTags(childhtml.Content(),
+                            "http://specialprojects.com");
+                        tempNodes.ForEach(nd =>
                         {
-                            container.Add(nd);
-                            activeQueue.Enqueue(new Node(nd));
-                        }
-                    });
+                            if (!container.Contains(nd))
+                            {
+                                container.Add(nd);
+                                activeQueue.Enqueue(new Node(nd));
+                            }
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        //Console.WriteLine("Error Occured: " + ex.Message);
+                    }
+                    double val = 1.0 - ((float)activeQueue.Count / (float)container.Count);
+                    int percent = (int) (val * 100.0);
+                    if(percent < 0) Console.Write($"\r0 %");
+                    else Console.Write($"\r{percent} %");
                 }
-                catch (Exception ex)
-                {
-                    //Console.WriteLine("Error Occured: " + ex.Message);
-                }
-            }
 
-            Console.WriteLine("Process Done");
-            container.Distinct().ToList();
-            List<Node> ctn = new List<Node>();
-            container.ForEach(item => ctn.Add(new Node(item)));
-            ctn.Sort();
-            SitemapCreator.Generate(ctn, "weekly", "0.5");
-            Console.ReadKey();
+                Console.WriteLine("\nCrawling Done");
+                container.Distinct().ToList();
+                List<Node> ctn = new List<Node>();
+                container.ForEach(item => ctn.Add(new Node(item)));
+                ctn.Sort();
+                string path = SitemapCreator.Generate(ctn, "weekly", "0.5");
+                Console.WriteLine($"Sitemap Generated: {path}");
+            }else
+            {
+                Console.WriteLine("Usage\n\n Sitemap.Generator <http(s)://website>\n\n" +
+                    "Specify the targeted website using http:// in front.");
+            }
+            
         }
     }
 }
--- Sitemap.Generator/Sitemap.Generator/SitemapCreator.cs
+++ Sitemap.Generator/Sitemap.Generator/SitemapCreator.cs
@@ -13,34 +13,39 @@ namespace Sitemap.Generator
     /// </summary>
     public static class SitemapCreator
     {
-        public static XNamespace x = "";
         public static XNamespace ns = "http://www.sitemaps.org/schemas/sitemap/0.9";
         public static XNamespace xs = "http://www.w3.org/2001/XMLSchema-instance";
+        public static XNamespace nxs = "http://www.sitemaps.org/schemas/sitemap/0.9";
 
         private static void AddNode(ref XDocument xmldoc, string link,
             string changef, string priority)
         {
-            XElement node = new XElement("url",
-                new XElement("loc", link),
-                new XElement("changefreq", changef),
-                new XElement("priority", priority)
+            XElement node = new XElement(nxs +"url",
+                new XElement(nxs +"loc", link),
+                new XElement(nxs +"changefreq", changef),
+                new XElement(nxs + "priority", priority)
                 );
             xmldoc.Root.Add(node);
         }
 
-        public static void Generate(List<Node> data, string changef,
+        public static string Generate(List<Node> data, string changef,
             string priority)
         {
+           
             XDocument sitemap = new XDocument(
                 new XDeclaration("1.0", "UTF-8", "no"),
-                new XElement(ns + "urlset",
+                new XElement( nxs + "urlset",
+                new XAttribute("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9"),
                     new XAttribute(XNamespace.Xmlns + "xsi", xs),
                     new XAttribute(xs + "SchemaLocation", 
                     "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd")
                     )
                 );
             data.ForEach(node => AddNode(ref sitemap, node.Link, changef, priority));
-            sitemap.Save(@"C:\Web Sites\main\xml\sitemap.xml", SaveOptions.None);
+            string path = @"C:\Web Sites\main\xml\sitemap.xml";
+            sitemap.Save(path, SaveOptions.None);
+            //sitemap.Save(@"C:\Web Sites\main\xml\sitemap.xml", SaveOptions.None);
+            return path;
         }
     }
 }
